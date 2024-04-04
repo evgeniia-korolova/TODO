e9718b8769df7300ebedcbf79fd87529d4526e4c
evgeniia-korolova <jkod@ukr.net>
2024-04-05 02:43:44 +0300

    feat : add basic layout, styles, add-task function

---
+++ main.js
@@ -0,0 +1,51 @@
+const taskInput = document.querySelector('#task-input');
+const addTaskBtn = document.querySelector('.add-task__btn');
+const taskList = document.querySelector('.task__list');
+const startMessage = document.querySelector('.start__text');
+const inputLabel = document.querySelector('.add-task__label');
+
+addTaskBtn.addEventListener('click', addTaskHandler)
+
+function createTask(task) {
+    let taskBlock = document.createElement('div');
+    taskBlock.classList.add('task-block')
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    
+    let taskText = document.createElement('p');
+    taskText.textContent = task;
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Del';
+
+    let changeTaskBtn = document.createElement('button');
+    changeTaskBtn.textContent = 'Change Task';
+
+    taskBlock.append(checkbox);
+    taskBlock.append(taskText);
+    taskBlock.append(deleteBtn);
+    taskBlock.append(changeTaskBtn);
+
+
+    return taskBlock;
+}
+
+function addTaskHandler() {
+    
+    if (taskInput.value) {
+        startMessage.classList.add('hidden');
+
+        let newTask = createTask(taskInput.value);
+        taskList.append(newTask);
+        taskInput.value = '';
+        // if (inputLabel.classList.contains('warning')) {
+        //     inputLabel.classList.remove('warning');
+        // }
+       
+    }
+    else { 
+        taskInput.placeholder = 'Add new task';
+        
+        // inputLabel.classList.add('warning');
+    }
+}
